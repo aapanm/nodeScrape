95d9378bf6f9efeb017d90e1a37acb33c7804f11
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-09-21 16:10:27 +0600

    doc

---
+++ Readme.md
@@ -0,0 +1,73 @@
+# Web Scraping using Node.js
+
+This Node.js script is designed to scrape data from the Otomoto website for Mercedes-Benz trucks produced since 2014 and using the Actros model. The script performs the following tasks:
+
+1. **Get Next Page URL**: `getNextPageUrl` function from [scrapping.js](/src/scrap/scrapping.js) finds the next pagination page link to scrape data
+
+2. **Add Items Function**: `getListIdWithUrl` from [scrapping.js](/src/scrap/scrapping.js) Fetches item URLs and item IDs (unique identifiers used on the portal) from the list page.
+
+3. **Get Total Ads Count Function**: `getTotalAdsCount` from [scrapping.js](/src/scrap/scrapping.js) provides information on how many total advertisements exist for the provided initial URL.
+
+4. **Scrape Truck Item Function**: `scrapeTruckItem` from [scrapping.js](/src/scrap/scrapping.js) scrapes individual truck ads and parses them into a structured format, including item ID, title, price, mileage, and power.
+
+5. **Scrape All Pages, All Ads**: `scrapeAllPages` from [scrapping.js](/src/scrap/scrapping.js) script runs the above functions to scrape data from all pages and all ads matching the specified criteria.
+
+##Data Storage
+
+All the scraped data are stored in the [data](/src/data/) folder in different files.
+
+1. `storeListIdUrl` from [dataStore.js](/src/dataStore.js) file stores all items id and urls in [ItemIdWithUrl.csv](/src/data/ItemIdWithUrl.csv) file
+
+1. `storeTruckInfo` from [dataStore.js](/src/dataStore.js) file stores all the truck information. Inital Page truck info are stored in [InitialPageTruckInfo.csv](/src/data/InitialPageTruckInfo.csv) and all scraped data are stored in [AllTruckInfo.csv](/src/data/AllTruckInfo.csv) file
+
+## Prerequisites
+
+Before running the script, make sure you have the following prerequisites:
+
+- Node.js installed on your machine.
+
+## Installation
+
+To install and set up the project on your local machine, follow these steps:
+
+1. Clone the Git repository using the following command:
+
+   ```bash
+   git clone https://github.com/aapanm/nodeScrape
+   ```
+
+2. Change to the project directory:
+
+   ```bash
+   cd nodeScrape
+   ```
+
+3. Install the project dependencies by running:
+
+   ```bash
+   npm install
+   ```
+
+## Running the Project
+
+Once you have completed the installation and configuration steps, you can run the project using the following command:
+
+```bash
+npm start
+```
+
+## Running the Project using Docker
+
+You can also run the project with docker. To make that happen first insatll Docker and then run the following commands in the terminal:
+
+1. Pull Docker image
+
+```bash
+Docker pull aapanm13/node_scrape
+```
+
+2. Run Docker image
+
+```bash
+Docker run aapanm13/node_scrape
+```
--- src/dataStore.js
+++ src/dataStore.js
@@ -1,35 +1,65 @@
 import { createObjectCsvWriter } from "csv-writer";
 
 async function storeListIdUrl(list) {
+  /*
+  list parameter expects data in shape of
+  [
+    {
+      'id': 123456,
+      'url': 'https://.....'
+    }
+  ]
+  */
+  // Create a CSV writer object to write data to a CSV file.
   const csvWriter = createObjectCsvWriter({
-    path: "src/data/ItemIdWithUrl.csv",
+    path: "src/data/ItemIdWithUrl.csv", // Specify the file path for the CSV file.
     header: [
-      { id: "id", title: "ID" },
+      { id: "id", title: "ID" }, // Define the CSV file headers (ID and URL).
       { id: "url", title: "URL" },
     ],
-    alwaysQuote: true,
+    alwaysQuote: true, // Ensure that values are enclosed in double quotes in the CSV file.
   });
 
   try {
+    // Write the records in the 'list' array to the CSV file using the CSV writer.
     await csvWriter.writeRecords(list);
-    console.log("data id and links recorded");
+
+    // Log a success message when the data is successfully recorded.
+    console.log("Data IDs and links recorded");
   } catch (e) {
+    // Handle any errors that occur during the writing process and log the error message.
     console.log(e);
   }
 }
 
-async function storeTruckInfo(turckInfo, type) {
+async function storeTruckInfo(truckInfo, type) {
+  /*
+  truckInfo parameter expects data in shape of
+  [
+    {
+      'id': 123456,
+      'title': 'abcsd',
+      'price': 1234,
+      'mileage': xyw km,
+      'power': 1200
+    }
+  ]
+  */
+
   let path;
 
+  // Determine the file path based on the 'type' parameter.
   if (type === "all") {
-    path = "src/data/AllTruckInfo.csv";
+    path = "src/data/AllTruckInfo.csv"; // Path for storing information of all trucks.
   }
 
   if (type === "initial") {
-    path = "src/data/InitialPageTruckInfo.csv";
+    path = "src/data/InitialPageTruckInfo.csv"; // Path for storing information of trucks from the initial page.
   }
+
+  // Create a CSV writer object to write data to the specified CSV file.
   const csvWriter = createObjectCsvWriter({
-    path,
+    path, // Use the determined file path.
     header: [
       { id: "id", title: "ID" },
       { id: "title", title: "TITLE" },
@@ -37,13 +67,17 @@ async function storeTruckInfo(turckInfo, type) {
       { id: "mileage", title: "MILEAGE" },
       { id: "power", title: "POWER" },
     ],
-    alwaysQuote: true,
+    alwaysQuote: true, // Ensure that values are enclosed in double quotes in the CSV file.
   });
 
   try {
-    await csvWriter.writeRecords(turckInfo);
-    console.log("truck info recorded");
+    // Write the truck information records in the 'truckInfo' array to the CSV file using the CSV writer.
+    await csvWriter.writeRecords(truckInfo);
+
+    // Log a success message when the truck information is successfully recorded.
+    console.log("Truck information recorded");
   } catch (e) {
+    // Handle any errors that occur during the writing process and log the error message.
     console.log(e);
   }
 }
